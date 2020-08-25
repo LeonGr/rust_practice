9219a98bb30b5e21db1e379b391c3ff1df58e367
LeonGr <leongrasmeijer@gmail.com>
2020-08-25 14:34:03 +0200

    Up to VecDeque

--- main.rs
+++ main.rs
@@ -123,4 +123,113 @@ fn main() {
     for i in vec![0, 1, 2, 3] {
         print!("{:?} ", i);
     }
+
+    println!();
+
+
+    /* Implementing structs and enums */
+    // Use impl to create method for enum or struct
+    // Regular methods: with &self or &mut self, use .
+    // Associated (or static) methods, no self, use :: (usually used to create new variables)
+
+    // #[] are rust attributes, e.g. #[derive(Debug)] to add {:?} printing
+
+    #[derive(Debug)]
+    struct Animal {
+        age: u8,
+        animal_type: AnimalType,
+    }
+
+    impl Animal {
+        fn new() -> Self {
+            Self {
+                age: 10,
+                animal_type: AnimalType::Cat,
+            }
+        }
+
+        fn change_to_dog(&mut self) {
+            println!("This animal is now a dog!");
+            self.animal_type = AnimalType::Dog;
+        }
+
+        fn change_to_cat(&mut self) {
+            println!("This animal is now a cat");
+            self.animal_type = AnimalType::Cat;
+        }
+
+        fn check_type(&self) {
+            match self.animal_type {
+                AnimalType::Dog => println!("This is a dog"),
+                AnimalType::Cat => println!("This is a cat"),
+            }
+        }
+    }
+
+
+    #[derive(Debug)]
+    enum AnimalType {
+        Cat,
+        Dog,
+    }
+
+    let mut new_animal = Animal::new();
+
+    new_animal.check_type();
+    new_animal.change_to_dog();
+    new_animal.check_type();
+    new_animal.change_to_cat();
+    new_animal.check_type();
+
+
+    /** Other Collections **/
+
+    /* HashMap (and BTreeMap) */
+    // Like a dictionary
+
+    use std::collections::HashMap;
+
+    let mut hash_map = HashMap::new();
+
+    hash_map.insert("Hello", "World");
+    hash_map.insert("int", "test");
+
+    println!("{:?}", hash_map.get("Hello"));
+
+    // Create HashMap from vector of tuples
+    let tuple_vector = vec![("Test", "1"), ("test", "2")];
+
+    let mut hash_from_vector: HashMap<&str, &str> = tuple_vector.into_iter().collect();
+
+    println!("{:?}", hash_from_vector["test"]);
+
+    // Can use .get("key") or ["key"], latter crashes if key is not in map
+
+    // BTreeMap is sorted verson
+
+    // with entry() you can check if something is in the map
+    println!("{:?}", hash_from_vector.entry("test"));
+
+    // You can use entry() to do things if there is no entry
+    hash_from_vector.entry("10").or_insert("a");
+
+    println!("{:?}", hash_from_vector["10"]);
+
+
+    /* HashSet and BTreeSet */
+    // Same as Map but only keys, value is ()
+    // Can use contains() or get()
+
+    /* BinaryHeap */
+    // Collection with largest item in the front (max heap)
+    // Hash push() to add and pop() to get largest item
+    // peek() get first item or None
+
+    use std::collections::BinaryHeap;
+
+    let heap: BinaryHeap<u8> = BinaryHeap::new();
+    println!("{:?}", heap.peek()); // -> None
+
+    /* VecDeque */
+    // A Vec that's good with pop() to remove the last element and remove() to remove the first element
 }
